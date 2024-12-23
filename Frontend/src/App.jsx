83d396210fb01cd1a62f0b607fcd0dafbd663f9f
Oluwole Samson <oluwolefsamson44@gmail.com>
2024-12-23 14:31:23 +0100

    setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   }
 
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-black w-full overflow-hidden">
       {!isHiddenPage || isAlwaysShowPage ? (
         <div className={`${styles.paddingX} ${styles.flexCenter}`}>
           <div className={`${styles.boxWidth}`}>
@@ -36,13 +36,13 @@ const App = () => {
           </div>
         </div>
       ) : null}
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-black ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <AppRouter />
         </div>
       </div>
       {!isHiddenPage || isAlwaysShowPage ? (
-        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
             <Footer />
           </div>
